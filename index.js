b4ced52d5d475dd207ac82275215116a4a459249
Anup Bishnoi <pixelsallover@gmail.com>
2018-01-24 09:00:34 +0000

    added logging, changed default aliveDuration for examples, fixed breaking bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const redis = global.redis || require('redis');
+const redis = require('redis');
 const log = require('debug')('vasco');
 
 let db;
@@ -14,40 +14,45 @@ function findDependencies(dependencies, mockDeps, done) {
     .filter(name => !!mockDeps[name])
     .forEach(name => depUrls[name] = mockDeps[name]);
 
-  const getURLKey = name => 'endpoints.' + name + '@' + dependencies[name];
+  const getServiceKey = name => name + '@' + dependencies[name];
+  const getURLKey = name => 'endpoints.' + getServiceKey(name);
   const getAliveKey = url => 'alive.' + url;
   const serviceDepNames = depNames.filter(name => !mockDeps[name]);
-  getActiveURLs(serviceDepNames, depUrls, done);
+  getAliveURLs(serviceDepNames, depUrls, done);
   
-  function getActiveURLs(deps, activeURLs, callback) {
-    if (!deps.length) { return callback(null, activeURLs); }
+  function getAliveURLs(deps, aliveURLs, callback) {
+    if (!deps.length) { return callback(null, aliveURLs); }
     
+    log('reqeusted dependencies:', deps);
     const urlBatch = db.batch();
     deps.forEach(name => urlBatch.srandmember(getURLKey(name)));
     urlBatch.exec((urlErr, urls) => {
       if (urlErr) { return callback(urlErr); }
+      log('found endpoints:', urls);
       for (let i = 0; i < urls.length; i++) {
-        if (!urls[i]) { return callback(new Error('No service:' + deps[urls[i]])); }
+        if (!urls[i]) { return callback(new Error('No url for: ' + deps[i])); }
       }
       
-      const activeBatch = db.batch();
-      urls.forEach(url => activeBatch.get(getAliveKey(url)));
-      activeBatch.exec((activeErr, activeValues) => {
-        if (activeErr) { return callback(activeErr); }
-        deps
-          .filter((name, index) => activeValues[index])
-          .forEach((name, index) => activeURLs[name] = urls[index]);
-
+      const aliveBatch = db.batch();
+      urls.forEach(url => aliveBatch.get(getAliveKey(url)));
+      aliveBatch.exec((aliveErr, aliveValues) => {
+        if (aliveErr) { return callback(aliveErr); }
+        log('alive status for endpoints:', aliveValues);
         const cleanUpBatch = db.batch();
-        deps
-          .map((name, index) => ({ name, alive: activeValues[index], url: urls[index] }))
-          .filter(dep => !dep.alive)
-          .forEach(dep => cleanUpBatch.srem(getURLKey(dep.name), dep.url));
+        deps.forEach((name, index) => {
+          if (aliveValues[index] === getServiceKey(name)) {
+            aliveURLs[name] = urls[index];
+          } else if (!aliveValues[index]) {
+            log('cleanup endpoint:', urls[index]);
+            cleanUpBatch.srem(getURLKey(name), urls[index]);
+          }
+        });
         cleanUpBatch.exec(cleanUpErr => {
           if (cleanUpErr) { return callback(cleanUpErr); }
-
-          const missingDeps = deps.filter((name, index) => !activeValues[index]);
-          getActiveURLs(missingDeps, activeURLs, callback);
+          const missingDeps = deps.filter((name, index) =>
+            aliveValues[index] !== getServiceKey(name));
+          log('request missing dependencies', missingDeps);
+          getAliveURLs(missingDeps, aliveURLs, callback);
         });
       });
     });
@@ -59,16 +64,17 @@ function register(url, pkg, done) {
   if (!pkg || !pkg.name || !pkg.version) { return done(new Error('Invalid package')); }
   const opts = pkg.vasco || {};
   const aliveDuration = opts.aliveDuration || 10; // seconds
+  const pkgNameVersion = pkg.name + '@' + pkg.version;
 
   connectDB();
   findDependencies(opts.dependencies || {}, opts.mocks || {}, (err, depUrls) => {
     log('found dependencies:', depUrls);
     if (err) { return done(err); }
-    const urlKey = 'endpoints.' + pkg.name + '@' + pkg.version;
+    const urlKey = 'endpoints.' + pkgNameVersion;
     db.sadd(urlKey, url, err => {
       if (err) { return done(err); }
       db.end(true);
-      log('registered:', url)
+      log('registered endpoint:', url);
       setServiceHealth(url, err => done(err, depUrls));
     });
   });
@@ -78,7 +84,7 @@ function register(url, pkg, done) {
     callback = callback || (err => { if (err) throw err; });
     
     connectDB();
-    db.set(key, true, 'EX', aliveDuration, err => {
+    db.set(key, pkgNameVersion, 'EX', aliveDuration, err => {
       if (err) { return callback(err); }
       db.end(true);
       setTimeout(setServiceHealth, aliveDuration * 1000, url);
